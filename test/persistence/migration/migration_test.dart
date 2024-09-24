e5e04f1c03e8c6ee6df65fc8e8cd17611c64b7a9
Mikita Kukavenka <nkukovenko@by.byelex.com>
2024-09-24 20:18:32 +0300

    tests

---
+++ test/persistence/migration/migration_test.dart
@@ -0,0 +1,29 @@
+import 'package:integration_test/integration_test.dart';
+import 'package:language_parser_desktop/util/sqlite.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:test/test.dart';
+
+const prefixPath = './test/persistence/migration/db';
+
+void main() {
+  
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  
+  test('Migration empty db', () async {
+    var fileDb = sqlite3.open('$prefixPath/00-version');
+
+    var db = sqlite3.copyIntoMemory(fileDb);
+    await migrate(db);
+
+    final fileVersion = getUserVersion(fileDb);
+    final version = getUserVersion(db);
+    expect(0, fileVersion);
+    expect(1, version);
+  });
+}
+
+int getUserVersion(Database db) {
+  return db
+      .select("PRAGMA user_version;")
+      .single['user_version'];
+}
