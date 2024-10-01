967bec20509399bbc115ebe9b63987ab9854854e
Mikita Kukavenka <nkukovenko@by.byelex.com>
2024-10-01 12:24:05 +0300

    Added link button

--- lib/components/buttons/t_button.dart
+++ lib/components/buttons/t_button.dart
@@ -8,21 +8,25 @@ class TButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 16, height: 0.0, fontFamily: 'Cousine', fontFeatures: [
-          FontFeature.tabularFigures(),
-        ]),
-        maxLines: 1,
-      ),
-      style: TextButton.styleFrom(
-        minimumSize: Size.zero,
-        padding: EdgeInsets.zero,
-        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        enableFeedback: false,
+    return GestureDetector(
+      onPanDown: (_) => onPressed!(),
+      child: TextButton(
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 16, height: 0.0, fontFamily: 'Cousine', fontFeatures: [
+            FontFeature.tabularFigures(),
+          ]),
+          maxLines: 1,
+        ),
+        onPressed: () => {},
+        style: TextButton.styleFrom(
+          minimumSize: Size.zero,
+          padding: EdgeInsets.zero,
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          enableFeedback: false,
+        ),
       ),
     );
+    // return
   }
 }
